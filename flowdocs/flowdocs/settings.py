627cecf9de1a6ae5bb43f6bfd6567848564a3c9a
Nimble-esolutions <86198548+Nimble-esolutions@users.noreply.github.com>
2025-09-15 13:12:17 +0530

    Update settings.py
    Add Static Root

--- flowdocs/flowdocs/settings.py
+++ flowdocs/flowdocs/settings.py
@@ -135,13 +135,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+#STATIC_URL = '/static/'
+#STATIC_ROOT = BASE_DIR / 'staticfiles'
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+
 
 # Additional static files directories
-STATICFILES_DIRS = [
-    BASE_DIR / 'core' / 'static',
-]
+#STATICFILES_DIRS = [
+  #  BASE_DIR / 'core' / 'static',
+#]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
